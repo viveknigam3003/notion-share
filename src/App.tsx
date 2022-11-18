73e999ddc7b767020376237b2d27e7e8a170f0f9
Vivek Nigam <vivek.nigam@rocketium.com>
2022-11-18 12:14:18 +0530

    feat: added icon text group to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/700.css';
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
+import { Box, Button, createStyles, Popover } from "@mantine/core";
+import { HiShare } from "react-icons/hi";
+import "./App.css";
+import IconTextGroup from "./components/IconTextGroup";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { classes } = useStyles();
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Box className={classes.root}>
+      <Popover
+        width={512}
+        trapFocus
+        position="bottom-start"
+        shadow="md"
+        transition={"fade"}
+      >
+        <Popover.Target>
+          <Button color="dark" rightIcon={<HiShare />}>
+            Share
+          </Button>
+        </Popover.Target>
+        <Popover.Dropdown>
+          <IconTextGroup />
+        </Popover.Dropdown>
+      </Popover>
+    </Box>
+  );
 }
 
-export default App
+export default App;
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    ".mantine-Popover-dropdown": {
+      borderRadius: "0.5rem",
+    },
+  },
+}));
