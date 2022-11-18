270c68e9dc5b21730ccdc8853bcbdffa4bf8416a
Vivek Nigam <vivek.nigam@rocketium.com>
2022-11-18 12:13:55 +0530

    feat: updated theme styles

--- src/theme/ButtonTheme.tsx
+++ src/theme/ButtonTheme.tsx
@@ -1,6 +1,7 @@
 export const ButtonTheme = {
   styles: {
     root: {
+      fontWeight: 500,
       padding: "0.5rem 1rem 0.5rem 1rem",
       borderRadius: "0.25rem",
     },
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -2,8 +2,17 @@ import { MantineThemeOverride } from "@mantine/core";
 import { ButtonTheme } from "./ButtonTheme";
 
 export const theme: MantineThemeOverride = {
-  fontFamily: 'Inter, Helvetica, Arial, sans-serif',
+  fontFamily: "Inter, Helvetica, Arial, sans-serif",
   components: {
     Button: ButtonTheme,
+  },
+  focusRingStyles: {
+    styles: (theme) => ({
+      outline: `2px solid ${theme.colors.violet[5]}`,
+      outlineOffset: "2px",
+    }),
+    inputStyles: (theme) => ({
+      outline: `2px solid ${theme.colors.violet[5]}`,
+    }),
   },
 };
