f65f8e63cac0622630b31905b5040ab8233f5a71
Vivek Nigam <vivek.nigam@rocketium.com>
2022-11-18 15:07:48 +0530

    feat: updated ModalSection props

--- src/components/ModalSection.tsx
+++ src/components/ModalSection.tsx
@@ -3,15 +3,27 @@ import React from "react";
 
 const ModalSection: React.FC<BoxProps & { children: React.ReactNode }> = ({
   children,
+  ...props
 }) => {
   const { classes } = useStyles();
-  return <Box className={classes.root}>{children}</Box>;
+  return (
+    <Box
+      className={classes.root}
+      pt="1rem"
+      pb="1rem"
+      pl="0.75rem"
+      pr="0.75rem"
+      {...props}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default ModalSection;
 
 const useStyles = createStyles((theme) => ({
   root: {
-    padding: "1rem 0.75rem 1rem 0.75rem",
+    // padding: "1rem 0.75rem 1rem 0.75rem",
   },
 }));
