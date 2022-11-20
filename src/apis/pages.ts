947d96e9111e704796ae187843e1e527723ab629
Vivek Nigam <vivek.nigam@rocketium.com>
2022-11-20 15:13:31 +0530

    feat: added remove permission logic

--- src/apis/pages.ts
+++ src/apis/pages.ts
@@ -22,10 +22,70 @@ export const updatePageConfig = (
   // update page config
   const newData = {
     ...data,
-    sharedWith: [
-      ...data.sharedWith,
-      ...sharedWith,
-    ],
+    sharedWith: [...data.sharedWith, ...sharedWith],
+  };
+
+  // replace page config in local storage
+  const allData = getPages();
+  const updatedData = allData.map((page: any) => {
+    if (page.id === pageId) {
+      return newData;
+    }
+    return page;
+  });
+
+  // update local storage
+  const stringifyData = JSON.stringify(updatedData);
+  localStorage.setItem(config.pageDB, stringifyData);
+};
+
+export const updatePagePermissionForUser = (
+  id: string,
+  permission: string,
+  pageId: string = "page-1"
+) => {
+  // get page config from local storage
+  const data = getPageConfig(pageId);
+
+  // update page config
+  const newData = {
+    ...data,
+    sharedWith: data.sharedWith.map((user: any) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          permission,
+        };
+      }
+      return user;
+    }),
+  };
+
+  // replace page config in local storage
+  const allData = getPages();
+  const updatedData = allData.map((page: any) => {
+    if (page.id === pageId) {
+      return newData;
+    }
+    return page;
+  });
+
+  // update local storage
+  const stringifyData = JSON.stringify(updatedData);
+  localStorage.setItem(config.pageDB, stringifyData);
+};
+
+export const removePagePermissionForUser = (
+  id: string,
+  pageId: string = "page-1"
+) => {
+  // get page config from local storage
+  const data = getPageConfig(pageId);
+
+  // update page config
+  const newData = {
+    ...data,
+    sharedWith: data.sharedWith.filter((user: any) => user.id !== id),
   };
 
   // replace page config in local storage
--- src/modules/AccessControlModal/InvitedUserList.tsx
+++ src/modules/AccessControlModal/InvitedUserList.tsx
@@ -1,11 +1,16 @@
 import { Group, Stack } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { getAccessData } from "../../apis/getAccessData";
+import {
+  removePagePermissionForUser,
+  updatePageConfig,
+  updatePagePermissionForUser,
+} from "../../apis/pages";
 import AccessSelector from "../../components/AccessSelector";
 import IconTextGroup from "../../components/IconTextGroup";
 import ModalSection from "../../components/ModalSection";
 import UserAvatar from "../../components/UserAvatar";
-import { PageShareObject } from "../../types";
+import { PageShareObject, PERMISSION_LEVEL } from "../../types";
 
 interface Props {}
 
@@ -16,6 +21,17 @@ const InvitedUserList = (props: Props) => {
     const data = getAccessData();
     setPageShareData(data);
   }, []);
+
+  const updatePermission = (id: string, permission: string) => {
+    if (permission === PERMISSION_LEVEL.NO_ACCESS) {
+      removePagePermissionForUser(id);
+    } else {
+      updatePagePermissionForUser(id, permission);
+    }
+
+    const data = getAccessData();
+    setPageShareData(data);
+  };
 
   return (
     <ModalSection pt="0">
@@ -29,7 +45,10 @@ const InvitedUserList = (props: Props) => {
               }
               leftNode={<UserAvatar src={user.avatar} alt={user.name} />}
             />
-            <AccessSelector value={user.permission} />
+            <AccessSelector
+              value={user.permission}
+              onChange={(v: PERMISSION_LEVEL) => updatePermission(user.id, v)}
+            />
           </Group>
         ))}
       </Stack>
